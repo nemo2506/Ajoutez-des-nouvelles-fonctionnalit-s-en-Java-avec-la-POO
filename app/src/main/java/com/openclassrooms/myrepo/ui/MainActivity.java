acb6406f2fc15ad71fb724d80433bef0ddea9b3f
MARC NAVARRO <navamarc@gmail.com>
2025-03-06 23:15:58 +0100

    ajout copyright

--- app/src/main/java/com/openclassrooms/myrepo/ui/MainActivity.java
+++ app/src/main/java/com/openclassrooms/myrepo/ui/MainActivity.java
@@ -1,3 +1,16 @@
+/*
+ * *
+ *  * Copyright (c) 2025, Marc. All rights reserved.
+ *  *
+ *  * Ce fichier est la propriété de Marc. Il ne peut être
+ *  * modifié, distribué ou utilisé sans autorisation explicite.
+ *  *
+ *  * @author Marc
+ *  * @version 1.0
+ *  * @since 2025-03-06
+ *
+ */
+
 package com.openclassrooms.myrepo.ui;
 
 
---
+++ app/src/test/java/com/openclassrooms/myrepo/ui/MainActivityTest.java
@@ -0,0 +1,22 @@
+/*
+ * *
+ *  * Copyright (c) 2025, Marc. All rights reserved.
+ *  *
+ *  * Ce fichier est la propriété de Marc. Il ne peut être
+ *  * modifié, distribué ou utilisé sans autorisation explicite.
+ *  *
+ *  * @author Marc
+ *  * @version 1.0
+ *  * @since 2025-03-06
+ *
+ */
+
+package com.openclassrooms.myrepo.ui;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+}
